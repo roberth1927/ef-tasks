b7657420677500ec88cf7454e4f12ecf033efd10
Roberth <45379245+roberth1927@users.noreply.github.com>
2023-08-24 09:41:51 -0500

    add animations

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,41 +2,56 @@
 @section('title', __('Dashboard'))
 @section('content')
 <div class="container-fluid">
-<div class="row justify-content-center">
-    <div class="col-md-12">
-        <div class="card">
-            <div class="card-header"><h5><span class="text-center fa fa-home"></span> @yield('title')</h5></div>
-            <div class="card-body">
-                <h5>Hola <strong>{{ Auth::user()->name }},</strong> {{ __('Vista general de los usuarios y las tareas ') }}</h5>
-                </br>
-                <hr>
-
-            <div class="row w-100">
-                <div class="col-md-3" x-data="{ hover: false }">
-                    <div class="card border-warning mx-sm-1 p-3" x-bind:class="{ 'hover:bg-yellow-200': hover }" @mouseenter="hover = true" @mouseleave="hover = false">
-                        <div class="card border-warning text-warning p-3 my-card" ><span class="text-center fa fa-users" aria-hidden="true"></span></div>
-                        <div class="text-warning text-center mt-3"><h4>Users</h4></div>
-                        <div class="text-warning text-center mt-2"><h1>{{ Auth::user()->count() }}</h1></div>
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5><span class="text-center fa fa-home"></span> @yield('title')</h5>
+                </div>
+                <div class="card-body">
+                    <h5>Hola <strong>{{ Auth::user()->name }},</strong> {{ __('Vista general de los usuarios y las tareas ') }}</h5>
+                    <hr>
+                    <div class="row w-100">
+                        <div class="col-md-3" x-data="{ scale: 1 }">
+                            <div class="card border-warning mx-sm-1 p-3"
+                                 x-bind:style="{ transform: 'scale(' + scale + ')', transition: 'transform 0.3s ease' }"
+                                 @mouseover="scale = 1.1"
+                                 @mouseleave="scale = 1">
+                                <div class="card border-warning text-warning p-3 my-card">
+                                    <span class="text-center fa fa-users" aria-hidden="true"></span>
+                                </div>
+                                <div class="text-warning text-center mt-3"><h4>Users</h4></div>
+                                <div class="text-warning text-center mt-2"><h1>{{ Auth::user()->count() }}</h1></div>
+                            </div>
+                        </div>
+                        <div class="col-md-3" x-data="{ scale: 1 }">
+                            <div class="card border-success mx-sm-1 p-3"
+                                 x-bind:style="{ transform: 'scale(' + scale + ')', transition: 'transform 0.3s ease' }"
+                                 @mouseover="scale = 1.1"
+                                 @mouseleave="scale = 1">
+                                <div class="card border-success text-success p-3 my-card">
+                                    <span class="text-center fa fa-luggage-cart" aria-hidden="true"></span>
+                                </div>
+                                <div class="text-success text-center mt-3"><h4>Tareas Completadas</h4></div>
+                                <div class="text-success text-center mt-2"><h1>{{ $completedTasksCount }}</h1></div>
+                            </div>
+                        </div>
+                        <div class="col-md-3" x-data="{ scale: 1 }">
+                            <div class="card border-danger mx-sm-1 p-3"
+                                 x-bind:style="{ transform: 'scale(' + scale + ')', transition: 'transform 0.3s ease' }"
+                                 @mouseover="scale = 1.1"
+                                 @mouseleave="scale = 1">
+                                <div class="card border-danger text-danger p-3 my-card">
+                                    <span class="text-center fa fa-person-booth" aria-hidden="true"></span>
+                                </div>
+                                <div class="text-danger text-center mt-3"><h4>Tareas Pendientes</h4></div>
+                                <div class="text-danger text-center mt-2"><h1>{{ $pendingTasksCount }}</h1></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="col-md-3" x-data="{ hover: false }">
-                    <div class="card border-success mx-sm-1 p-3" x-bind:class="{ 'hover:bg-green-200': hover }" @mouseenter="hover = true" @mouseleave="hover = false">
-                        <div class="card border-success text-success p-3 my-card"><span class="text-center fa fa-luggage-cart" aria-hidden="true"></span></div>
-                        <div class="text-success text-center mt-3"><h4>Tareas Completadas</h4></div>
-                        <div class="text-success text-center mt-2"><h1>{{ $completedTasksCount }}</h1></div>
-                    </div>
-                </div>
-                <div class="col-md-3" x-data="{ hover: false }">
-                    <div class="card border-danger mx-sm-1 p-3" x-bind:class="{ 'hover:bg-red-200': hover }" @mouseenter="hover = true" @mouseleave="hover = false">
-                        <div class="card border-danger text-danger p-3 my-card" ><span class="text-center fa fa-person-booth" aria-hidden="true"></span></div>
-                        <div class="text-danger text-center mt-3"><h4>Tareas Pendientes</h4></div>
-                        <div class="text-danger text-center mt-2"><h1>{{ $pendingTasksCount }}</h1></div>
-                    </div>
-                </div>
-             </div>
+            </div>
         </div>
     </div>
 </div>
-</div>
 @endsection
-
--- resources/views/livewire/tasks/view.blade.php
+++ resources/views/livewire/tasks/view.blade.php
@@ -8,13 +8,20 @@
                             <h4><i class="fab fa-laravel text-info"></i> Listado de tareas</h4>
                         </div>
                         @if (session()->has('message'))
-                        <div wire:poll.4s class="btn btn-sm btn-success" style="margin-top:0px; margin-bottom:0px;"> {{ session('message') }} </div>
+                            <div wire:poll.4s class="btn btn-sm btn-success" style="margin-top:0px; margin-bottom:0px;">
+                                {{ session('message') }} </div>
                         @endif
-                        <div>
-                            <input wire:model='keyWord' type="text" class="form-control" name="search" id="search" placeholder="Filtrar Tareas">
+                        <div x-data="{ scale: 1 }"
+                            x-bind:style="{ transform: 'scale(' + scale + ')', transition: 'transform 0.3s ease' }"
+                            @mouseover="scale = 1.2" @mouseleave="scale = 1">
+                            <input wire:model='keyWord' type="text" class="form-control" name="search"
+                                id="search" placeholder="Filtrar Tareas">
                         </div>
-                        <div class="btn btn-sm btn-info" data-bs-toggle="modal" data-bs-target="#createDataModal">
-                            <i class="fa fa-plus"></i>  Crear Tarea
+                        <div x-data="{ scale: 1 }" class="btn btn-sm btn-info" data-bs-toggle="modal"
+                            data-bs-target="#createDataModal"
+                            x-bind:style="{ transform: 'scale(' + scale + ')', transition: 'transform 0.3s ease' }"
+                            @mouseenter="scale = 1.2" @mouseleave="scale = 1">
+                            <i class="fa fa-plus"></i> Crear Tarea
                         </div>
                     </div>
                 </div>
@@ -28,32 +35,46 @@
                                     <td>#</td>
                                     <th>Nombre</th>
                                     <th>Estado</th>
-                                    <td></td>
+                                    <th>ACCIONES</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 @forelse($tasks as $row)
-                                <tr x-data="{ hover: false }" @mouseenter="hover = true" @mouseleave="hover = false" :class="{ 'bg-yellow-100': hover }">
-                                    <td>{{ $loop->iteration }}</td>
-                                    <td>{{ $row->name }}</td>
-                                    <td>{{ $row->status }}</td>
-                                    <td width="90" class="text-center">
-                                        <div class="btn-group">
-                                            @if ($row->status == 'pendiente')
-                                            <a data-bs-toggle="modal" data-bs-target="#updateDataModal" class="btn btn-sm btn-success margin-" wire:click="edit({{$row->id}})">
-                                                <i class="fa fa-edit"></i>
-                                            </a>
-                                            @endif
-                                            <a class="btn btn-sm btn-danger " onclick="confirm('Confirm Delete Task id {{$row->id}}? \nDeleted Tasks cannot be recovered!')||event.stopImmediatePropagation()" wire:click="destroy({{$row->id}})">
-                                                <i class="fa fa-trash"></i>
-                                            </a>
-                                        </div>
-                                    </td>
-                                </tr>
+                                    <tr  x-data="{ scale: 1 }"
+                                        x-bind:style="{ transform: 'scale(' + scale + ')',transition: 'transform 0.3s ease' }"
+                                        @mouseenter="scale = 1" @mouseleave="scale = 1"
+                                        >
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $row->name }}</td>
+                                        <td>{{ $row->status }}</td>
+                                        <td width="90" class="text-center">
+                                            <div class="btn-group">
+                                                @if ($row->status == 'pendiente')
+                                                    <a data-bs-toggle="modal"
+                                                        x-data="{ scale: 1 }"
+                                                        x-bind:style="{ transform: 'scale(' + scale + ')',transition: 'transform 0.3s ease' }"
+                                                        @mouseenter="scale = 1.2" @mouseleave="scale = 1"
+                                                        data-bs-target="#updateDataModal"
+                                                        class="btn btn-sm btn-success margin-"
+                                                        wire:click="edit({{ $row->id }})">
+                                                        <i class="fa fa-edit"></i>
+                                                    </a>
+                                                @endif
+                                                <a class="btn btn-sm btn-danger"
+                                                    onclick="confirm('Confirm Delete Task id {{ $row->id }}? \nDeleted Tasks cannot be recovered!')||event.stopImmediatePropagation()"
+                                                    wire:click="destroy({{ $row->id }})" x-data="{ scale: 1 }"
+                                                    x-bind:style="{ transform: 'scale(' + scale + ')',
+                                                        transition: 'transform 0.3s ease' }"
+                                                    @mouseenter="scale = 1.2" @mouseleave="scale = 1">
+                                                    <i class="fa fa-trash"></i>
+                                                </a>
+                                            </div>
+                                        </td>
+                                    </tr>
                                 @empty
-                                <tr>
-                                    <td class="text-center" colspan="100%">No data Found </td>
-                                </tr>
+                                    <tr>
+                                        <td class="text-center" colspan="100%">No data Found </td>
+                                    </tr>
                                 @endforelse
                             </tbody>
                         </table>
@@ -64,4 +85,3 @@
         </div>
     </div>
 </section>
-
